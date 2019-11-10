d34fb5752f8c884cc944ad34755ae5dc2d25bd48
Tyler Kim <tyler.ys.kim@gmail.com>
2019-11-10 13:57:59 -0500

    Update hackingToolsAndMedia.py

--- hackingToolsAndMedia.py
+++ hackingToolsAndMedia.py
@@ -6,7 +6,7 @@ response = input("Remove Hacking Tools? Y/N")
 if response == "Y":
         for hackingProgram in hackingTools:
                 print("Removing ", hackingProgram)
-                subprocess.call(["sudo", "apt", "purge", hackingProgram + "*", "-y"])
+                subprocess.call(["sudo", "apt-get", "purge", hackingProgram + "*", "-y"])
 
 ################## Find all media files and delete
 mediaFileTypes = ["mp3", "mov", "mp4", "png", "pdf", "gif", "jpeg", "tiff", "bmp", "aav", "aac", "wav", "wma", "dts", "mpeg-1", "mpeg-2", "mpeg-4", "avi", "avchd"]

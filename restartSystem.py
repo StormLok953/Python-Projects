d42d6572db4087232c2b3c259f76a688d9e2d908
Tyler Kim <tyler.ys.kim@gmail.com>
2019-11-10 15:49:04 -0500

    Delete restartSystem.py

--- restartSystem.py
+++
@@ -1,6 +0,0 @@
-import subprocess, os, time, sys, json, grp, pwd
-
-################### Restart sysctl
-response = input("Restart sysctl? Y/N")
-if response == "Y":
-         os.system("sudo sysctl -p")

07d8bb5a41b8740a34c6d93abf973c72f8c5ad9e
Tyler Kim <tyler.ys.kim@gmail.com>
2019-11-10 14:38:11 -0500

    Update restartSystem.py

--- restartSystem.py
+++ restartSystem.py
@@ -4,12 +4,3 @@ import subprocess, os, time, sys, json, grp, pwd
 response = input("Restart sysctl? Y/N")
 if response == "Y":
          os.system("sudo sysctl -p")
-        
-
-################## Find all media files and delete
-mediaFileTypes = ["mp3", "mov", "mp4", "png", "pdf", "gif", "jpeg", "tiff", "bmp", "aav", "aac", "wav", "wma", "dts", "mpeg-1", "mpeg-2", "mpeg-4", "avi", "avchd"]
-response = input("Find all media files? Y/N")
-if response == "Y":
-        for fileTypes in mediaFileTypes:
-                subprocess.call(["find", "/", "-type", "f", "-iname", "*." + fileTypes])
-

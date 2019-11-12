2e8da4650c5410b204b4aec837eebd24a5dc18dd
Tyler Kim <tyler.ys.kim@gmail.com>
2019-11-11 21:25:36 -0800

    Update hackingToolsAndMedia.py

--- hackingToolsAndMedia.py
+++ hackingToolsAndMedia.py
@@ -9,11 +9,10 @@ if response == "Y":
                 subprocess.call(["sudo", "apt-get", "purge", hackingProgram + "*", "-y"])
 
 ################## Find all media files and delete
-mediaFileTypes = ["mp3", "mov", "mp4", "png", "pdf", "gif", "jpeg", "tiff", "bmp", "aav", "aac", "wav", "wma", "dts", "mpeg-1", "mpeg-2", "mpeg-4", "avi", "avchd"]
+mediaFileTypes = ["mp3", "mov", "mp4", "png", "pdf", "gif", "jpeg", "tiff", "bmp", "avi"]
 response = input("Find all media files? Y/N")
 if response == "Y":
         for fileTypes in mediaFileTypes:
                 subprocess.call(["find", "/", "-type", "f", "-iname", "*." + fileTypes])
 
 ################# Secure Ports
-

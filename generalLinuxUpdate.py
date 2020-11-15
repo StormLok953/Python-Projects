515628518daed81beeb8ca4484025672931b95e6
Tazering <tyler.ys.kim@gmail.com>
2020-11-14 21:46:57 -0800

    Update

--- generalLinuxUpdate.py
+++ generalLinuxUpdate.py
@@ -25,82 +25,93 @@ if os.geteuid() != 0:
 	exit()
 
 ############# Create backup file
-os.system("mkdir ~/backup")
+def initiation():
+	os.system("mkdir ~/backup")
 
 
 ############# Make sure preliminary problems are solved
-response = raw_input("Are the Forensic Questions solved?")
-print("Moving On")
+	response = input("Are the Forensic Questions solved?")
+	print("Moving On")
 
 
 ############# Enable the Firewall
-response = raw_input("Would you like to enable UFW Firewall? y or n")
-if response is 'y' or response is 'Y':
-	os.system("sudo ufw enable")
+def enableUFWFirewall():
+	response = input("Would you like to enable UFW Firewall? y or n")
+	if response is 'y' or response is 'Y':
+		os.system("sudo ufw enable")
 
 
 ############# Install openssh-server
-response = raw_input("Would you like to download openssh-server? y or n")
-if response is 'y' or response is 'Y':
-	os.system("sudo apt install openssh-server")
+def installOpenSSHServer():
+	response = input("Would you like to download openssh-server? y or n")
+	if response is 'y' or response is 'Y':
+		os.system("sudo apt install openssh-server")
 
 
 ############# Disable Root Login
-print("Prerequisites: openssh-server must be installed")
-response = raw_input("Would you like to disable root login? y or n")
-if response is 'y' or response is 'Y':
-	os.system("sudo cp /etc/ssh/sshd_config ~/backup")
-	listLines = []
-	with open("/etc/ssh/sshd_config", "rt") as readOnlyFile:
-		listLines = list(readOnlyFile)
-	for line in listLines:
-		print(line)
-	
-	with open("/etc/ssh/sshd_config", "wt") as disableRootLoginFile:
+def disableRootLogin():
+	print("Prerequisites: openssh-server must be installed")
+	response = input("Would you like to disable root login? y or n")
+	if response is 'y' or response is 'Y':
+		os.system("sudo cp /etc/ssh/sshd_config ~/backup")
+		listLines = []
+		with open("/etc/ssh/sshd_config", "rt") as readOnlyFile:
+			listLines = list(readOnlyFile)
 		for line in listLines:
-			if ("PermitRootLogin" in line) and ("#" not in line):
-				disableRootLoginFile.write("PermitRootLogin no\n")
-			else:
-				disableRootLoginFile.write(line)
+			print(line)
+		with open("/etc/ssh/sshd_config", "wt") as disableRootLoginFile:
+			for line in listLines:
+				if ("PermitRootLogin" in line) and ("#" not in line):
+					disableRootLoginFile.write("PermitRootLogin no\n")
+				else:
+					disableRootLoginFile.write(line)
 
-############# Enforce Password Policy 
-response = raw_input("Would you like to enforce password complexity and length? y or n")
-if response is 'y' or response is 'Y':
-	os.system("sudo apt-get install libpam-cracklib")
-	os.system("sudo cp /etc/pam.d/common-password ~/backup")
-	listLines = []
-	with open("/etc/pam.d/common-password", "rt") as readOnlyFile:
-		listLines = list(readOnlyFile)
-	for line in listLines:
-		print(line)
-
-	with open("/etc/pam.d/common-password", "wt") as passwordComplexityAndLength:
+############## Enforce Password Policy 
+def enforcePasswordPolicy():
+	response = input("Would you like to enforce password complexity and length? y or n")
+	if response is 'y' or response is 'Y':
+		os.system("sudo apt-get install libpam-cracklib")
+		os.system("sudo cp /etc/pam.d/common-password ~/backup")
+		listLines = []
+		with open("/etc/pam.d/common-password", "rt") as readOnlyFile:
+			listLines = list(readOnlyFile)
 		for line in listLines:
-			if ("pam_unix.so" in line) and ("#" not in line):
-				passwordComplexityAndLength.write("password	[success=1 default=ignore]	pam_unix.so obsure sha512 minlen=8 remember=5\n") 
-			elif ("pam_cracklib.so" in line) and ("#" not in line):
-				passwordComplexityAndLength.write("password	requisite		pam_cracklib.so retry=3 minlen=8 ucredit=-1 lcredit=-1 dcredit=-1 ocredit=-1\n")
-			else:
-				passwordComplexityAndLength.write(line)
+			print(line)
+
+		with open("/etc/pam.d/common-password", "wt") as passwordComplexityAndLength:
+			for line in listLines:
+				if ("pam_unix.so" in line) and ("#" not in line):
+					passwordComplexityAndLength.write("password	[success=1 default=ignore]	pam_unix.so obsure sha512 minlen=8 remember=5\n") 
+				elif ("pam_cracklib.so" in line) and ("#" not in line):
+					passwordComplexityAndLength.write("password	requisite		pam_cracklib.so retry=3 minlen=8 ucredit=-1 lcredit=-1 dcredit=-1 ocredit=-1\n")
+				else:
+					passwordComplexityAndLength.write(line)
 	
 
-############ Force SSH to use Public Key Authentication
-print("Prerequisites: openssh-server must be installed")
-response = raw_input("Force SSH to require public key authentication? ")
-if response is 'y' or response is 'Y':
-	os.system("sudo cp /etc/ssh/sshd_config ~/backup")
-	listLines = []
-	with open("/etc/ssh/sshd_config", "rt") as readOnlyFile:
-		listLines = list(readOnlyFile)
-	for line in listLines:
-		print(line)
-
-	with open("/etc/ssh/sshd_config", "wt") as publicKeyAuthentication:
+############# Force SSH to use Public Key Authentication
+def forceSSHToUsePublicKeyAuthentication():
+	print("Prerequisites: openssh-server must be installed")
+	response = input("Force SSH to require public key authentication? ")
+	if response is 'y' or response is 'Y':
+		os.system("sudo cp /etc/ssh/sshd_config ~/backup")
+		listLines = []
+		with open("/etc/ssh/sshd_config", "rt") as readOnlyFile:
+			listLines = list(readOnlyFile)
 		for line in listLines:
-			if ("PasswordAuthentication yes" in line):
-				publicKeyAuthentication.write("PasswordAuthentication no\n")
-			elif ("PubkeyAuthentication no" in line):
-				publicKeyAuthentication.write("PubkeyAuthentication yes\n")
-			else:
-				publicKeyAuthentication.write(line)	
+			print(line)
+
+		with open("/etc/ssh/sshd_config", "wt") as publicKeyAuthentication:
+			for line in listLines:
+				if ("PasswordAuthentication yes" in line):
+					publicKeyAuthentication.write("PasswordAuthentication no\n")
+				elif ("PubkeyAuthentication no" in line):
+					publicKeyAuthentication.write("PubkeyAuthentication yes\n")
+				else:
+					publicKeyAuthentication.write(line)	
 	
+initiation()
+enableUFWFirewall()
+installOpenSSHServer()
+disableRootLogin()
+enforcePasswordPolicy()
+forceSSHToUsePublicKeyAuthentication()
--- hacking_tools_and_media.py
+++ hacking_tools_and_media.py
@@ -1,20 +1,25 @@
 import subprocess, os, time, sys, json, grp, pwd
 
 ############ Delete All Hacking Tools
-hacking_tools = ["zenmap", "nmap", "telnet", "hydra", "john", "nitko", "freeciv", "ophcrack", "kismet", "minetest", "netcat", "vsftpd"]
+def purgeHackingTools():
+	hacking_tools = ["zenmap", "nmap", "telnet", "hydra", "john", "nitko", "freeciv", "ophcrack", "kismet", "minetest", "netcat", "vsftpd", "metasploit", "ncat"]
 
-response = raw_input("Would you like to remove all the hacking tools? y or n")
-if response is 'y' or response is 'Y':
-	for hacking_tool in hacking_tools:
-		print("Removing ", hacking_tool)
-		subprocess.call(["sudo", "apt-get", "purge", hacking_tool + "*", "-y"])
+	response = input("Would you like to remove all the hacking tools? y or n")
+	if response is 'y' or response is 'Y':
+		for hacking_tool in hacking_tools:
+			print("Removing ", hacking_tool)
+			subprocess.call(["sudo", "apt-get", "purge", hacking_tool + "*", "-y"])
 
 
 ############ Final All Media Files and Delete
-media_file_extensions = ["mp3", "mov", "mp4", "png", "pdf", "gif", "jpeg", "tiff", "bmp", "avi"]
+def purgeMediaFiles():
+	media_file_extensions = ["mp3", "mov", "mp4", "png", "pdf", "gif", "jpeg", "tiff", "bmp", "avi"]
 #TODO find uppercase 
-response = raw_input("Would you like to detect all the media file types? y or n")
-if response is 'y' or response is 'Y':
-	for media_file_extension in media_file_extensions:
-		print("Finding ", media_file_extension)
-		subprocess.call(["find", "/home", "-type", "f", "-iname", "*." + media_file_extension])
+	response = input("Would you like to detect all the media file types? y or n")
+	if response is 'y' or response is 'Y':
+		for media_file_extension in media_file_extensions:
+			print("Finding ", media_file_extension)
+			subprocess.call(["find", "/home", "-type", "f", "-iname", "*." + media_file_extension])
+
+purgeHackingTools()
+purgeMediaFiles()
